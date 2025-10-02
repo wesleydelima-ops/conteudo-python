76432288788ffcc149214faf942ca2751e65494e
Aluno SENAI <Aluno@SN-368517.edu.fiesc.com.br>
2025-10-02 16:48:13 -0300

    conteudo sobre print e variaveis

---
+++ aula1.py
@@ -0,0 +1,9 @@
+# como nosso primeiro
+print("olá mundo!")
+print("lógica","de","programação",sep=" ",end="\n")
+print("vai","imprimir","em","baixo?",sep="\n")
+print("vai",end=" ")
+print("imprimir",end=" ")
+print("na",end=" ")
+print("mesma",end=" ")
+print("linha",end=" ")
---
+++ aula1_variaveis.py
@@ -0,0 +1,10 @@
+#variaveis são
+
+# Podemos definir variáveis com o nome dela, seguido pelo
+#sinal de igual, que significa "atribuir" e depois pelo valor.
+# nome = "wesley"<-Crie uma variavel nome que recebe o valor "wesley"
+salario = 14.00
+
+print("O salário do cauã é",salario)
+print("ele é do job")
+
